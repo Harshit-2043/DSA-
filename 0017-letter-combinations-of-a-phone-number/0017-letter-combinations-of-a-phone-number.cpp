fa5e59e4f4a729d7e876a1ee63bdf8fe9e9af50b
HARSHIT  JHA <swastikdjha@gmail.com>
2026-08-17 01:11:08 +0530

    Time: 0 ms (100%), Space: 9.6 MB (12.71%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void backtrack(string& digits, int idx, string curr,
+                   vector<string>& mp) {
+
+        if (idx == digits.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        string letters = mp[digits[idx] - '0'];
+
+        for (char ch : letters) {
+            backtrack(digits, idx + 1, curr + ch, mp);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+
+        vector<string> mp = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        backtrack(digits, 0, "", mp);
+
+        return ans;
+    }
+};
